fc7116c1044d744ef77cae2fc4e4ddb277bdaca9
Sip <sipg789@gmail.com>
2022-05-11 10:26:38 -0500

    added api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+
+const userRoutes = require('./user-routes.js');
+
+
+router.use('/users', userRoutes);
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
